5ab247e53a8677075e39e19018074d85e2f45aec
Niko <niko40@gmail.com>
2019-10-23 20:47:44 -0400

    Designing better MeshBuffer class interface to handle dynamic allocations.

--- Source/Header/MeshBuffer.h
+++ Source/Header/MeshBuffer.h
@@ -18,6 +18,11 @@ namespace vk2d {
 
 class MeshBuffer {
 public:
+	struct MeshOffsets {
+		VkDeviceSize								vertex_byte_offset;
+		VkDeviceSize								index_byte_offset;
+	};
+
 	MeshBuffer(
 		VkDevice									device,
 		const VkPhysicalDeviceLimits			&	physicald_device_limits,
@@ -26,52 +31,88 @@ public:
 
 	~MeshBuffer();
 
-	void											PushBackVertex(
-		Vertex										new_vertex );
-
-	void											PushBackVertices(
-		const std::vector<Vertex>				&	new_vertices );
-
-	void											PushBackIndex(
-		uint32_t									new_index );
-
-	void											PushBackIndices(
+	// Pushes mesh into render list, dynamically allocates new buffers
+	// if needed, binds the new buffers to command buffer if needed
+	// and adds vertex and index data to host visible buffer.
+	// Returns mesh offsets of whatever buffer object this mesh was
+	// put into, needed when recording a Vulkan draw command.
+	MeshOffsets										CmdPushMesh(
+		VkCommandBuffer								command_buffer,
+		const std::vector<Vertex>				&	new_vertices,
 		const std::vector<uint32_t>				&	new_indices );
 
-	void											PushBackIndices(
+	// Pushes mesh into render list, dynamically allocates new buffers
+	// if needed, binds the new buffers to command buffer if needed
+	// and adds vertex and index data to host visible buffer.
+	// Returns mesh offsets of whatever buffer object this mesh was
+	// put into, needed when recording a Vulkan draw command.
+	MeshOffsets										CmdPushMesh(
+		VkCommandBuffer								command_buffer,
+		const std::vector<Vertex>				&	new_vertices,
 		const std::vector<VertexIndex_2>		&	new_indices );
 
-	void											PushBackIndices(
+	// Pushes mesh into render list, dynamically allocates new buffers
+	// if needed, binds the new buffers to command buffer if needed
+	// and adds vertex and index data to host visible buffer.
+	// Returns mesh offsets of whatever buffer object this mesh was
+	// put into, needed when recording a Vulkan draw command.
+	MeshOffsets										CmdPushMesh(
+		VkCommandBuffer								command_buffer,
+		const std::vector<Vertex>				&	new_vertices,
 		const std::vector<VertexIndex_3>		&	new_indices );
 
-	bool											CmdUploadToGPU(
+	bool											CmdUploadMeshDataToGPU(
 		VkCommandBuffer								command_buffer );
 
-	// This gets the current amount of vertices already pushed in
-	size_t											GetCurrentVertexCount();
+	// Gets the total amount of individual meshes that have been pushed so far.
+	size_t											GetPushedMeshCount();
 
-	// This gets the current amount of indices already pushed in
-	size_t											GetCurrentIndexCount();
+	// This gets the total amount of vertices already pushed in
+	size_t											GetTotalVertexCount();
+
+	// This gets the total amount of indices already pushed in
+	size_t											GetTotalIndexCount();
 
 private:
-	bool											ResizeStagingBuffer(
-		VkDeviceSize								new_size );
+	// Keeps track of allocation blocks, we'll need a method of dynamically
+	// allocating more memory as the rendering gets more complicated.
+	// This struct is POD and completely managed by the MeshBuffer object.
+	// OPTIMIZE MEMORY: Might be a good idea to create VertexBufferBlock
+	//   and IndexBufferBlock and individually manage both instead of
+	//   storing both vertices and indices within a single buffer object
+	//   with pre-set size limit on both halfs of the buffer.
+	struct BufferBlock {
+		std::vector<Vertex>							vertices						= {};
+		std::vector<uint32_t>						indices							= {};
 
-	bool											ResizeDeviceBuffer(
-		VkDeviceSize								new_size );
+		VkBuffer									staging_buffer					= {};
+		VkBuffer									device_buffer					= {};
 
-	// TODO: Look into replacing these with std::deque, just need a new method of copying to staging buffer
-	std::vector<Vertex>								vertices						= {};
-	std::vector<uint32_t>							indices							= {};
+		PoolMemory									staging_buffer_memory			= {};
+		PoolMemory									device_buffer_memory			= {};
 
-	VkBuffer										staging_buffer					= {};
-	VkBuffer										device_buffer					= {};
+		VkDeviceSize								buffer_index_size_left			= {};
+		VkDeviceSize								buffer_vertex_size_left			= {};
+	};
+	struct ReserveSpaceResult {
+		BufferBlock								*	buffer_block					= {};	// nullptr if failed
+		MeshOffsets									offsets							= {};
+	};
 
-	PoolMemory										staging_buffer_memory			= {};
-	PoolMemory										device_buffer_memory			= {};
+	// Returns only reference, buffer block is stored inside this object
+	BufferBlock									*	AllocateBufferBlock();
 
-	VkDeviceSize									current_staging_buffer_size		= {};
-	VkDeviceSize									current_device_buffer_size		= {};
+	// Takes only reference, buffer block is stored inside this object
+	void											FreeBufferBlock(
+		BufferBlock								*	buffer_block );
+
+	ReserveSpaceResult								ReserveSpaceForMesh(
+		VkDeviceSize								vertex_count,
+		VkDeviceSize								index_count );
+
+
+
+	std::vector<std::unique_ptr<BufferBlock>>		buffer_blocks					= {};
 
 	VkDevice										device							= {};
 	VkPhysicalDeviceLimits							physicald_device_limits			= {};
