1fc9c77f1c753cfac5939a823144a95ea3ad5a86
Niko Kauppi <niko40@gmail.com>
2019-12-25 21:30:19 -0500

    Added freetype external library. Changed file structure to more easily manage libraries.

--- ExternalLibraries/cmake_VisualStudio2019.bat
+++ ExternalLibraries/cmake_VisualStudio2019.bat
@@ -1,4 +1,14 @@
-mkdir glfw-3.3-VisualStudio2019
-cd glfw-3.3-VisualStudio2019
-cmake -D BUILD_SHARED_LIBS=OFF -D GLFW_BUILD_EXAMPLES=OFF -D GLFW_BUILD_TESTS=OFF -D GLFW_BUILD_DOCS=OFF -D GLFW_VULKAN_STATIC=ON -G "Visual Studio 16 2019" -A "x64" ..\glfw-3.3
+mkdir VisualStudio2019
+cd VisualStudio2019
+
+mkdir glfw-3.3
+cd glfw-3.3
+cmake -D BUILD_SHARED_LIBS=OFF -D GLFW_BUILD_EXAMPLES=OFF -D GLFW_BUILD_TESTS=OFF -D GLFW_BUILD_DOCS=OFF -D GLFW_VULKAN_STATIC=ON -G "Visual Studio 16 2019" -A "x64" ..\..\glfw-3.3
+cd ..
+
+mkdir freetype-2.10.1
+cd freetype-2.10.1
+cmake -G "Visual Studio 16 2019" -A "x64" ..\..\freetype-2.10.1
+cd ..
+
 pause
