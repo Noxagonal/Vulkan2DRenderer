02906292b0aea3cc47a7dc6ad0a9eae52aa8071d
Niko Kauppi <10339735+Noxagonal@users.noreply.github.com>
2022-12-12 18:31:34 -0500

    Added RenderPipeline description.

--- include/interface/render_pipeline/RenderPipeline.hpp
+++ include/interface/render_pipeline/RenderPipeline.hpp
@@ -19,6 +19,13 @@ class RenderPipelineImpl;
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @brief		Render pipeline allows customizing every step of the rendering process.
+///
+///				Render pipeline allows user to implement post-processing, lighting passes, and special effects.
+///
+///				Render pipeline includes an arbitary number of GBuffers in where intermediate and final renders are stored, as
+///				well as render passes which describe how rendering is done and how GBuffers are combined to create the final
+///				render.
 class RenderPipeline
 {
 	friend class vk2d_internal::InstanceImpl;
